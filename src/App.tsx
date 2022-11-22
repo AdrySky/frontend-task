a4a5f6d5b1c37459846d5e901ac1ecf0782ecc4b
AdrySky <adryaqil@gmail.com>
2022-11-22 13:00:42 +0800

    testing deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2 className='App-title'>Event listing</h2>
+        <h3>Testing deploy</h3>
       </header>
       <EventListing />
     </div>
